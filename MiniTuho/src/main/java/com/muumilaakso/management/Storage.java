e337b5a8dd3a0d1e1deca5ce0221969c704a5bb9
Henri Ylikotila <kelesti@gmail.com>
2012-04-20 13:01:24 +0300

    xml output

--- MiniTuho/src/main/java/com/muumilaakso/management/Storage.java
+++ MiniTuho/src/main/java/com/muumilaakso/management/Storage.java
@@ -5,7 +5,13 @@
  */
 package com.muumilaakso.management;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  *
@@ -14,9 +20,12 @@ import java.util.ArrayList;
 public class Storage {
 
     ArrayList<Reference> refs;
+    File store = new File("store.txt");
+    FileWriter output; 
 
-    public Storage() {
+    public Storage() throws IOException {
         refs = new ArrayList<Reference>();
+        output = new FileWriter(store);
     }
 
     /**
@@ -52,5 +61,31 @@ public class Storage {
      */
     public ArrayList<Reference> getRefs() {
         return refs;
+    }
+
+    /**
+     * Exports the arraylist to XML-file for easy importing action.
+     */
+    public void exportXML() throws IOException {
+        String out = "";
+        for (Reference reference : refs) {
+            HashMap<String, String> attr = reference.getAttr();
+
+            Iterator it = attr.entrySet().iterator();
+
+            while (it.hasNext()) {
+                Map.Entry pairs = (Map.Entry) it.next();
+                String attrKey = pairs.getKey().toString();
+                String attrValue = pairs.getValue().toString();
+                out += "<" + attrKey + ">" + ":" + attrValue + "</" + attrKey + ">\n";
+                output.append(out);
+            }
+        }
+    }
+
+    /**
+     * Imports the database from a XML-file.
+     */
+    public void importXML() {
     }
 }
