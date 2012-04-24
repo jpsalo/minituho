f0d0df07ba36e1f852902f1e9655b8eaeef595b9
Taru Airola <taru.airola@helsinki.fi>
2012-04-24 10:36:30 +0300

    Searchtestin muokkaus

--- MiniTuho/src/test/java/com/muumilaakso/management/SearchTest.java
+++ MiniTuho/src/test/java/com/muumilaakso/management/SearchTest.java
@@ -28,16 +28,25 @@ public class SearchTest extends TestCase {
         this.testRef = new Reference();
         this.testStorage = new Storage();
         this.testSearch = new Search(refs);
+        refs.add(testRef);
+        testRef.setAddress("Muumitalo");
+        testRef.setTitle("Muumilaakson tarinat");
     }
 
     /**
      * Test of listMatching(String haet) method, of class Search.
      */
     public void testlistMatching() {
-        refs.add(testRef);
-        testRef.setAddress("Muumitalo");
-        testRef.setTitle("Muumilaakson tarinat");
         Reference tulos = testSearch.listMatching("Muumilaakso").get(0);
+        
+        assertTrue(tulos.getTitle().contains("Muumilaakson tarinat"));       
+    }    
+    
+    /**
+     * Test of listMatching(String haet) method, of class Search.
+     */
+    public void testlistMatching2() {
+        Reference tulos = testSearch.listMatching("Muumilaakson tarinat",true).get(0);       
         
         assertTrue(tulos.getTitle().contains("Muumilaakson tarinat"));       
     }    
